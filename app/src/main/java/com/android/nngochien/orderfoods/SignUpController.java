eb0f5ad99cf716c247039b07ff2db52415d8a127
HienNgoc <ngochien.hien95@gmail.com>
2018-09-12 21:19:44 +0700

    sign up ok

--- app/src/main/java/com/android/nngochien/orderfoods/SignUpController.java
+++ app/src/main/java/com/android/nngochien/orderfoods/SignUpController.java
@@ -1,10 +1,19 @@
 package com.android.nngochien.orderfoods;
 
+import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.nngochien.orderfoods.Model.User;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class SignUpController extends AppCompatActivity {
 
@@ -22,21 +31,37 @@ public class SignUpController extends AppCompatActivity {
         edtPhone = (EditText)findViewById(R.id.edtPhone);
         edtPassword = (EditText)findViewById(R.id.edtPassword);
 
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final DatabaseReference tableUser = database.getReference("User");
+
         btnSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //Validate empty fields
-                if(isEmpty(edtName)) {
-                    edtName.setError("This field is required!");
-                }
-                if(isEmpty(edtPassword)) {
-                    edtPassword.setError("This field is required!");
-                }
-                if(isEmpty(edtConfirmPassword)) {
-                    edtConfirmPassword.setError("This field is required!");
-                }
-                if(isEmpty(edtPhone)) {
-                    edtPhone.setError("This field is required!");
+                final ProgressDialog mDialog = new ProgressDialog(SignUpController.this);
+                mDialog.setMessage("Please wait...");
+
+
+                if(validateInput()) {
+                    tableUser.addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+                            //Check user had already exist
+                            if(dataSnapshot.child(edtPhone.getText().toString()).exists()) {
+                                Toast.makeText(SignUpController.this, "Phone Number had already exist!", Toast.LENGTH_SHORT).show();
+                            } else {
+                                mDialog.show();
+                                User newUser = new User(edtName.getText().toString(), edtPassword.getText().toString());
+                                tableUser.child(edtPhone.getText().toString()).setValue(newUser);
+                                mDialog.dismiss();
+                                Toast.makeText(SignUpController.this, "Register successfully, you can sign in now...", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(DatabaseError databaseError) {
+
+                        }
+                    });
                 }
             }
         });
@@ -46,4 +71,35 @@ public class SignUpController extends AppCompatActivity {
     private boolean isEmpty(EditText input) {
         return input.getText().toString().isEmpty();
     }
+
+    private boolean validateInput() {
+        boolean valid = true;
+
+        if(isEmpty(edtConfirmPassword) || isEmpty(edtName) || isEmpty(edtPassword) || isEmpty(edtPhone)) {
+            valid = false;
+            //Validate empty fields
+            if(isEmpty(edtName)) {
+                edtName.setError("This field is required!");
+            }
+            if(isEmpty(edtPassword)) {
+                edtPassword.setError("This field is required!");
+            }
+            if(isEmpty(edtConfirmPassword)) {
+                edtConfirmPassword.setError("This field is required!");
+            }
+            if(isEmpty(edtPhone)) {
+                edtPhone.setError("This field is required!");
+            }
+        }
+
+        if(valid) {
+            if(!edtPassword.getText().toString().equals(edtConfirmPassword.getText().toString())) {
+                valid = false;
+                edtConfirmPassword.setError("The password must be the same!");
+            }
+        }
+
+        return valid;
+    }
+
 }
